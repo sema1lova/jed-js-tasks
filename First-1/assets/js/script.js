9a91b2559f646e73e2019aecc8221848e860e4b1
samavm <88936498+samavm@users.noreply.github.com>
2023-06-04 21:16:45 +0400

    first js task

---
+++ First-1/assets/js/script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/*
+// first task
+
+let promtName = prompt("Zəhmət olmasa adınızı daxil edin: ");
+
+console.log("Salam " + promtName);
+
+*/
+
+/*
+// second task
+
+let promtName = prompt("Zəhmət olmasa adınızı daxil edin: ");
+
+let promtAge =prompt("Zəhmət olmasa yaşınızı daxil edin: ");
+
+if (promtAge >= 18) console.log("Siz sistemə daxil oldunuz ");
+
+else console.log("Siz sistemə daxil ola bilmərsiz !");
+
+*/
+
+
+
+/*
+// third task
+
+let promtName = prompt("Zəhmət olmasa adınızı daxil edin: ");
+let promtGender =prompt("Zəhmət olmasa cinsinizi daxil edin: ");
+
+if (promtGender == "k" || promtGender == "K") console.log(`Salam ${promtName} cənab`); 
+
+else if (promtGender == "q" || promtGender == "Q") console.log(`Salam ${promtName} xanım`);    
+
+else console.log("Düzgün format daxil edin");
+
+*/
+
+
+
+/*
+// fourth task Calculator
+
+const number1 = prompt("Birinci rəqəmi daxil edin: ")
+const number2 = prompt("Ikinci rəqəmi daxil edin: ")
+const operator = prompt("Operatoru daxil edin: ( + , - , * , / )")
+
+if (!IsNaN(number1)) {
+    if (!IsNaN(number2)) {
+        if (operator == "+") console.log(`Cavab: ${number1 + number2}`);
+
+        else if(operator == "-") console.log(`Cavab: ${number1 - number2}`);
+
+        else if(operator == "*") console.log(`Cavab: ${number1 * number2}`);
+        
+        else if(operator == "/") console.log(`Cavab: ${number1 / number2}`);
+
+        else console.log("Düzgün operatoru daxil edin ! ");
+    }
+    else console.log("İkinci rəqəm düzgün deyil !");
+}  
+else console.log("Birinci rəqəm düzgün deyil !");
+
+*/
